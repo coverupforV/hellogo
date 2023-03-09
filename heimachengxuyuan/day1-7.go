36928ed8dc936679383650e196333655b23d3167
coverupforV <pcw760404931@163.com>
2023-03-09 20:09:58 +0800

    modified:   heimachengxuyuan/day1-7.go 	new file:   heimachengxuyuan/day1-8.go

--- heimachengxuyuan/day1-7.go
+++ heimachengxuyuan/day1-7.go
@@ -7,4 +7,14 @@ func main() {
 	var ch byte
 	ch = 97
 	fmt.Printf("%c", ch)
+
+	//字符，单引号
+	ch = 'a' //也可
+
+	//字符串类型,也可以使用自动推导类型
+	var str1 string
+	str1 = "123"
+	fmt.Println(str1)
+	//字符串长度len（）
+	fmt.Println(len(str1))
 }
---
+++ heimachengxuyuan/day1-8.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	fmt.Scanf("%d", &a) //标准化输入，注意取址符号
+	//或者这样也对  fmt.Scan(&a)
+
+}
